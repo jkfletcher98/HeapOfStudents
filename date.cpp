bb6b835e9732e47c6524aeabb2a724fd04548178
jkfletcher98 <jkfletcher@bsu.edu>
2025-02-19 02:59:08 +0000

    Started date.cpp

---
+++ date.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+Date::Date(){
+	std::string dateString = ; //not sure
+	int month = 0;
+	int day = 0;
+	int year = 0;
+} // end constructor
+
+void Date::init(dateString){
+
+} //end init
+
+void printDate(){
+	monthNames[] = {null, January, February, March, April, May, June, July, August, September, October, November, December};
+
