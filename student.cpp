8546cc3a4e1cf66108f69bd6b970f2ecc274365d
jkfletcher98 <jkfletcher@bsu.edu>
2025-02-25 19:49:03 +0000

    Started student.cpp

---
+++ student.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "student.h"
+#include "address.h"
+#include "date.h"
+
+Student::Student(){
+	std::string studentString = "";
+	std::string fName = "";
+	std::string lName = "";
+
+	Address* address;
+	Date* birthDate;
+	Date* gradDate;
+
+	int credit = 0;
+} // end constructor
+
+void Student::init(std::string studentString){
+
+} // end init
+
+void Student::printStudent(){
+
+} // end printStudent
